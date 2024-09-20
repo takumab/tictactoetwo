f57e1a3a5505e3b77bd252f0d5cd1322a8d5245c
takumab <takumab@gmail.com>
2024-09-20 18:17:16 +0100

    Refactor to state design pattern

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,36 +1,91 @@
 enum Player {
-    X = 'X',
-    O = 'O',
+  X = "X",
+  O = "O",
+}
+
+interface State {
+  currentPlayer: Player;
+  switchPlayer(): void;
+}
+
+class PlayerXState implements State {
+  readonly currentPlayer: Player;
+  private game: TicTacToeGame;
+  constructor(game: TicTacToeGame) {
+    this.game = game;
+    this.currentPlayer = Player.X;
+  }
+  switchPlayer(): void {
+    console.log(
+      `Switching from player ${this.currentPlayer} to ${this.game.oState.currentPlayer}`,
+    );
+    this.game.setState(this.game.oState);
+  }
+}
+
+class PlayerOState implements State {
+  readonly currentPlayer: Player = Player.O;
+  private game: TicTacToeGame;
+  constructor(game: TicTacToeGame) {
+    this.game = game;
+    this.currentPlayer = Player.O;
+  }
+
+  switchPlayer(): void {
+    console.log(
+      `Switching from player ${this.currentPlayer} to ${this.game.xState.currentPlayer}`,
+    );
+    this.game.setState(this.game.xState);
+  }
 }
 
 class TicTacToeGame {
-    private currentPlayer: Player = Player.X;
-    getCurrentPlayer() {
-        return this.currentPlayer
-    }
+  public currentState: State;
+  public oState: State;
+  public xState: State;
 
-    play() {
-        this.switchPlayer();
-    }
+  constructor() {
+    this.xState = new PlayerXState(this);
+    this.oState = new PlayerOState(this);
+    this.currentState = new PlayerXState(this);
+  }
 
-    private switchPlayer() {
-        if (this.currentPlayer === Player.X) {
-            this.currentPlayer = Player.O
-        }
-    }
+  getCurrentPlayer() {
+    return this.currentState.currentPlayer;
+  }
+
+  play() {
+    this.switchPlayer();
+  }
+
+  private switchPlayer() {
+    this.currentState.switchPlayer();
+  }
+
+  setState(state: State) {
+    this.currentState = state;
+  }
 }
 
-describe('Tic Tac Toe Should', () => {
-    test("make 'X' the first player", () => {
-        let game = new TicTacToeGame();
-        let currentPlayer = game.getCurrentPlayer()
-        expect(currentPlayer).toBe(Player.X);
-    })
+describe("Tic Tac Toe Should", () => {
+  test("make 'X' make first move", () => {
+    let game = new TicTacToeGame();
+    let currentPlayer = game.getCurrentPlayer();
+    expect(currentPlayer).toBe(Player.X);
+  });
 
-    test("make 'O' the second player", () => {
-        let game = new TicTacToeGame();
-        game.play()
-        let currentPlayer = game.getCurrentPlayer()
-        expect(currentPlayer).toBe(Player.O);
-    })
+  test("make 'O' place the second mark", () => {
+    let game = new TicTacToeGame();
+    game.play();
+    let currentPlayer = game.getCurrentPlayer();
+    expect(currentPlayer).toBe(Player.O);
+  });
+
+  test("make 'X' place the third mark", () => {
+    let game = new TicTacToeGame();
+    game.play();
+    game.play();
+    let currentPlayer = game.getCurrentPlayer();
+    expect(currentPlayer).toBe(Player.X);
+  });
 });
